67942fc8deea1fb4b9e3d4a7b880ce6febed8b43
Patri <patri@malvidopazo.es>
2022-02-08 09:35:16 +0100

    add socialmedia links

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,6 +1,7 @@
-import { Instagram, Twitter, LinkedIn } from '@material-ui/icons';
+import { Instagram, Twitter, LinkedIn, GitHub } from '@material-ui/icons';
 import '../styles/layout/Main.scss';
 import imgPatri from '../images/imgPatrisiluetbackground.png';
+import { Link } from 'react-router-dom';
 
 const Main = () => {
   return (
@@ -13,9 +14,18 @@ const Main = () => {
             <p>Jr.Frontend Developer</p>
 
             <div className="icons">
-              <Twitter className="icon" />
-              <Instagram className="icon" />
-              <LinkedIn className="icon" />
+              <a href="https://twitter.com/patrimalvido">
+                <Twitter className="icon" />
+              </a>
+              <a href="https://www.instagram.com/patrimalvido/">
+                <Instagram className="icon" />
+              </a>
+              <a href="https://www.linkedin.com/in/patri-malvido-paz%C3%B3-9a4632230/">
+                <LinkedIn className="icon" />
+              </a>
+              <a href="https://github.com/PatriMalvido">
+                <GitHub className="icon" />
+              </a>
             </div>
 
             <div className="buttons">
