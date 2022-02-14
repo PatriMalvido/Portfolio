b763359a606df7cf0b0d3dcbd66f12d7f1288211
Patri <patri@malvidopazo.es>
2022-02-14 11:07:49 +0100

    add ion-icons

--- src/components/Section.js
+++ src/components/Section.js
@@ -1,24 +1,41 @@
 import '../styles/layout/Section.scss';
-import imgPatri from '../images/imgPatri.jpeg';
+import imgCode from '../images/imgcode.jpg';
 
 const Section = () => {
   return (
     <div className="section">
       <div className="section__container">
         <div className="section__img">
-          <img src={imgPatri} alt="" />
+          <img src={imgCode} alt="" />
         </div>
 
         <div className="section__content">
           <h1>Who is Patri</h1>
-          <p>Después de 14 años dedicándome al comercio y a la decoración/planificación de eventos, decidí darle un giro a mi trayectoria profesional.
+          <p>
+            Después de 14 años dedicándome al comercio y a la
+            decoración/planificación de eventos,
           </p>
-          <p>Fue entonces cuando retomé mis estudios y me encontré con el fantástico mundo frontend.</p>
+          <p>decidí darle un giro a mi trayectoria profesional.</p>
+
+          <p>
+            Fue entonces cuando retomé mis estudios y me encontré con el
+            fantástico mundo frontend.
+          </p>
           <p>Mi reto: aprender todos los días algo nuevo</p>
-          <p> Junior fronted and backend developer</p>
+          {/* <p> Junior fronted and backend developer</p>
           <p>JavaSript developer</p>
           <p>React js Developer</p>
-          <p>Node js, Express js and SQLite developer</p>
+          <p>Node js, Express js and SQLite developer</p> */}
+
+          <div className="section__icons">
+            <ion-icon name="logo-html5"></ion-icon>
+            <ion-icon name="logo-css3"></ion-icon>
+            <ion-icon name="logo-sass"></ion-icon>
+            <ion-icon name="logo-react"></ion-icon>
+            <ion-icon name="logo-javascript"></ion-icon>
+            <ion-icon name="logo-nodejs"></ion-icon>
+            <ion-icon name="logo-github"></ion-icon>
+          </div>
         </div>
       </div>
     </div>
