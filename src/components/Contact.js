520a0e4d859be6d1b53e1d354b36234da63580a1
Patri <patri@malvidopazo.es>
2022-02-21 09:04:13 +0100

    add favicon

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -21,7 +21,7 @@ const Contact = () => {
           <input type="text" placeholder='Email' />
           <input type="text" placeholder='Message' className='message'/> */}
 
-          <button type='submit' >Send (in testing)</button>
+          <button type='submit' >Send</button>
       </div>
       </form>
     </div>
